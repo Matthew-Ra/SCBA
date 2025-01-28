93414ca943d1bcb6cf7198e5041c31595222ddfc
Matthew-Ra <r.rampergash@gmail.com>
2025-01-28 15:56:14 -0500

    Add files via upload

--- Main/main.cpp
+++ Main/main.cpp
@@ -5,8 +5,10 @@
 
 #include "GooberTest.h" 
 #include "BeastieBall.h"
-#include "BGTest.h"
+#include "Lv1Test.h"
 #include "CollisionTest.h" 
+
+using namespace std;
 
 //TO DO
 // Clean Up: Wall Class functions, functions for specific tasks
@@ -28,8 +30,8 @@ bool inBetween(int x, int lowR, int highR){
 uint16 cen_x = 128;
 uint16 cen_y = 100;
 
-int bg_x = 0;
-int bg_y = 0;
+int bg_x = -32;
+int bg_y = 32;
 
 //angle calculation
 float hyp = 0;
@@ -37,7 +39,7 @@ float adj = 0;
 float opp = 0;
 
 uint16 coll_x = 20;
-uint16 coll_y = 40;
+uint16 coll_y = 75;
 
 // initialize variables
 uint16_t kyHeld;
@@ -54,36 +56,52 @@ public:
         int x2;
         int y1;
         int y2;
-        int dir;
-       int wallPoints(int x, int xx, int y, int yy, int d) { 
+       int wallPoints(int x, int xx, int y, int yy) { 
         this->x1 = x;
         this->x2 = xx;
-        this->y1 = y;
+        this->y1 = y - 32;
         this->y2 = yy;
-        this->dir = d;
 
         return (0);
         }
 
         //Collision check
-        bool collision() {
+        bool collisionVertical() {
                 if (inBetween(cen_x - 16, this->x1, this->x2)){
-                        if (inBetween(cen_y + 16, this->y2, this->y1)){
-                                printf("Collision!!");
+                        if (inBetween(cen_y + 16, this->y1, this->y2)){
+                                if ( (inBetween(cen_y + 16, this->y1 - 8, this->y1 + 8)) or (inBetween(cen_y - 16, this->y2 - 8, this->y2 + 8))){
                                 return(true);
+                                }
+                        } else return(false);
+                } else return(false);
+        };
+
+        bool collisionHorizontal() {
+                if (inBetween(cen_x - 16, this->x1, this->x2)){
+                        if (inBetween(cen_y + 16, this->y1, this->y2)){
+                                //CHANGE LINE BELOW TO POINT DIST!!!
+                                if ( (inBetween(cen_x + 16, this->x2, this->x2 + 8)) or (inBetween(cen_x + 16, this->x1 - 8, this->x1 + 8))){
+                                return(true);
+                                }
                         } else return(false);
                 } else return(false);
         };
 };
 
 //established wall array as well as an array of coordinate points for the wall objects
+wallClass walls1[3];
 
-wallClass walls1[2];
-
-int Test0[2][5]{
-        {0, 256, 30, 0, 2},
-        {20, 100, 80, 30, 2}
+int Test1[2][4]{
+        {0, 256, 0, 32},
+        {32, 128, 32, 64}
 };
+
+int Test0[3][4]{
+        {0, 256, 0, 32},
+        {128, 256, 32, 128},
+        {32, 160, 160, 224}
+};
+
 
 
 void angleCalculation() {
@@ -97,15 +115,13 @@ void angleCalculation() {
 //Speed vector at a starting at center
         ang = acos(adj/(hyp * 1.0)); 
         ang_rad = degreesToAngle(ang*(180/M_PI) - 90);
-//Update console message
-        printf("opp = [%d]\n", opp);
-        printf("adj [%d]\n", adj);
+
 };
 
 void graphicsUpdate() {
 
-    s32 adjacentSpeed = (adj/7);
-    s32 oppositeSpeed = (opp/7);
+    s32 adjacentSpeed = (adj/15);
+    s32 oppositeSpeed = (opp/15);
 
 //**Enemy Collision check**
         if (pointDist(coll_x, coll_y, cen_x, cen_y) <= 32){
@@ -124,46 +140,98 @@ void graphicsUpdate() {
         bg_x -= adjacentSpeed;
         coll_x += adjacentSpeed;
 
-
         bg_y -= oppositeSpeed;
         coll_y += oppositeSpeed;
 
+        int wallSize = (sizeof(walls1)/sizeof(walls1[0])) - 1;
 
-        for (int i = 0; i <= 1; i++){
-                walls1[i].x1 += adjacentSpeed;
-                walls1[i].x2 += adjacentSpeed;
+                for (int i = 0; i <= wallSize; i++){
+                        walls1[i].x1 += adjacentSpeed;
+                        walls1[i].x2 += adjacentSpeed;
 
-                walls1[i].y1 += oppositeSpeed;
-                walls1[i].y2 += oppositeSpeed;
+                        walls1[i].y1 += oppositeSpeed;
+                        walls1[i].y2 += oppositeSpeed;
 
-                if (walls1[i].collision()) { //CANCEL movement if collision detected
-                        bg_y += oppositeSpeed;
-                        coll_y -= oppositeSpeed;
-                        for (int b = 0; b <= 1; b++){
-                                walls1[b].y1 -= oppositeSpeed;
-                                walls1[b].y2 -= oppositeSpeed;
-                        }
+                        if (walls1[i].collisionVertical()) { //CANCEL movement if collision detected
+                                bg_y += oppositeSpeed;
+                                coll_y -= oppositeSpeed;
+                                for (int w = 0; w <= wallSize; w++){
+                                        walls1[w].y1 -= oppositeSpeed;
+                                        walls1[w].y2 -= oppositeSpeed;
+                                };
+                        };
 
+                        if (walls1[i].collisionHorizontal()) { //CANCEL movement if collision detected
+                                bg_x += adjacentSpeed;
+                                coll_x -= adjacentSpeed;
+                                for (int w = 0; w <= wallSize; w++){
+                                        walls1[w].x1 -= adjacentSpeed;
+                                        walls1[w].x2 -= adjacentSpeed;
+                                };
+                        };
                 };
-
         };
-}
-
-
 
         //Refresh visuals
         bgUpdate();
         scanKeys();
         oamUpdate(&oamMain);
 
-        };
+};
 
+class sceneClass {
+        public:
+        int sceneID;   //Unique Scene Identifier
+        int sceneType; //0 = Menu, 1 = Level, 2 = Cutscene
+        //Level Data
+        char wallArray;
+
+        void sceneInit(int ID, int type){
+                this->sceneID = ID;
+                this->sceneType = type;
+        }
+
+        void levelInit(char array){
+                this->wallArray = array;
+        }
+};
+
+void levelSetup(sceneClass scene) {
+
+        //What needs to be added? 
+        //Scene ID, type (level/cutscene/menu), switch statement to handle each case
+
+//Which set of walls should be loaded?
+        switch(scene.sceneID){
+        case 0:
+                for (int w = 0; w <= (sizeof(Test0)/sizeof(Test0[0])); w++){
+                        walls1[w].wallPoints(Test0[w][0], Test0[w][1], Test0[w][2], Test0[w][3]);
+                }
+        break;
+        case 1:
+
+                for (int e = 0; e <= (sizeof(Test1)/sizeof(Test1[0])); e++){
+                        walls1[e].wallPoints(Test1[e][0], Test1[e][1], Test1[e][2], Test1[e][3]);
+                }
+        break;
+        }
+};
         
 
 
 //**MAIN **//
 
 int main(int argc, char *argv[]){ 
+
+//Establish Scenes, then run level set up at first scene
+sceneClass levelTest;
+levelTest.sceneInit(0, 1);
+//Array name as string, can I use that string to refer to the array? Load array info into a new array? Use old system, but load position array based off of SceneID
+
+sceneClass levelQuiz;
+levelQuiz.sceneInit(1, 1);
+
+levelSetup(levelTest);
 
 
 videoSetMode(MODE_1_2D);
@@ -182,28 +250,26 @@ oamInit(&oamSub, SpriteMapping_1D_32, false);
 
 //Initialize Background MAIN Data
 int bg = bgInit(0,BgType_Text8bpp, BgSize_T_256x256, 0, 1);
-dmaCopy(BGTestTiles, bgGetGfxPtr(bg), BGTestTilesLen);
-dmaCopy(BGTestMap, bgGetMapPtr(bg), BGTestMapLen);
-dmaCopy(BGTestPal, BG_PALETTE, BGTestPalLen);
-/*
+dmaCopy(Lv1TestTiles, bgGetGfxPtr(bg), Lv1TestTilesLen);
+dmaCopy(Lv1TestMap, bgGetMapPtr(bg), Lv1TestMapLen);
+dmaCopy(Lv1TestPal, BG_PALETTE, Lv1TestPalLen);
+
 //Initialize Background SUB Data
 int bgSb = bgInitSub(0,BgType_Text8bpp, BgSize_T_256x256, 0, 1);
-dmaCopy(BGTestTiles, bgGetGfxPtr(bgSb), BGTestTilesLen);
-dmaCopy(BGTestMap, bgGetMapPtr(bgSb), BGTestMapLen);
-dmaCopy(BGTestPal, BG_PALETTE_SUB, BGTestPalLen);
-*/
+dmaCopy(Lv1TestTiles, bgGetGfxPtr(bgSb), Lv1TestTilesLen);
+dmaCopy(Lv1TestMap, bgGetMapPtr(bgSb), Lv1TestMapLen);
+dmaCopy(Lv1TestPal, BG_PALETTE_SUB, Lv1TestPalLen);
+
 
 //Initialize Collision MAIN Data
 int Col = bgInit(3,BgType_Rotation, BgSize_R_256x256, 1, 2);
 dmaCopy(CollTestTiles, bgGetGfxPtr(Col), CollTestTilesLen);
 dmaCopy(CollTestMap, bgGetMapPtr(Col), CollTestMapLen);
 
-/*
 //Initialize Collision SUB Data
 int ColSb = bgInitSub(3,BgType_Rotation, BgSize_R_256x256, 1, 2);
 dmaCopy(CollTestTiles, bgGetGfxPtr(ColSb), CollTestTilesLen);
 dmaCopy(CollTestMap, bgGetMapPtr(ColSb), CollTestMapLen);
-*/
 
 //Set aside space for sprite and copy tile and pallete info to main engine
 u16 *gfxMain = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
@@ -240,21 +306,19 @@ oamSet(&oamMain, 1,
         true, false,
         true);
 
-               walls1[0].wallPoints(Test0[0][0], Test0[0][1], Test0[0][2], Test0[0][3], Test0[0][4]);
-               walls1[1].wallPoints(Test0[1][0], Test0[1][1], Test0[1][2], Test0[1][3], Test0[1][4]);
-
-
-        while(1){ //This is just an infinite loop, until something stops it
+        while(true){ //This is just an infinite loop, until something stops it
                 swiWaitForVBlank();
         //Background Initalization
                 bgSetScroll(bg, bg_x, bg_y);
                 bgSetScroll(Col, bg_x/2, bg_y/2);
-                //bgSetScroll(bgSb, bg_x, bg_y - 192);
-
+                bgSetScroll(bgSb, bg_x, bg_y - 192);
         //Graphics manipulation
                 oamRotateScale(&oamMain, 0, -ang_rad, scale, scale);
                 oamSetXY(&oamMain, 1, coll_x, coll_y);
                 kyHeld = keysHeld();
+                if (kyHeld & KEY_LEFT){
+                        levelSetup(levelQuiz);
+                };
 
                 graphicsUpdate();
     //flush visuals
